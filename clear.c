be8a0464996704cb1a484cd89fc9f473b0c6c204
farizputradandi <31751153+farizputradandi@users.noreply.github.com>
2017-11-26 21:13:39 +0700

    ternyata kalo gapake exit tar eror.

--- clear.c
+++ clear.c
@@ -1,11 +1,12 @@
 
-#include "stdio.h"
-#include "stdlib.h"
 #include "types.h"
 #include "stat.h"
 #include "user.h"
 #include "fs.h"
-int main(int argc, char *argv[])
+
+int
+main(int argc, char *argv[])
 {
   printf(1, "\033[2J\033[1;1H\n");
+  exit();
 }

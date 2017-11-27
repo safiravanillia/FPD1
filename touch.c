a229354bfce2c81c4bc7e66d3030d684e6ad204f
alcredo <simanjuntakalcredo@gmail.com>
2017-11-28 06:50:28 +0700

    repairing code of touch.c

--- touch.c
+++ touch.c
@@ -1,23 +1,17 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "fcntl.h"
 
 int main(int argc,char *argv[])
 {
 	int i;
-	if(argc<2)
-	{
-		printf(2,"Usage : touch files...\n");
-		exit();
-	}
+	int inputfile;
 
-	for(i = 1;i<argc;i++)
+	for(i = 1;i<=argc;i++)
 	{
-		if(mkfile(argv[i]) < 0)
-		{
-			printf(2,"touch : %s failed to create\n",argv[i]);
-			break;
-		}
+		inputfile = open(argv[i],O_CREATE | O_RDWR);
+		close(inputfile);
 	}
 	exit();
 }

a0177d3a51b63689bbe1d4b3f8d6f9a49cf62582
alcredo <simanjuntakalcredo@gmail.com>
2017-12-03 18:37:11 +0700

    cek dan rapikan code

--- cp.c
+++ cp.c
@@ -6,41 +6,41 @@
 
 char buf[512];
 
-void
-cat(int fd1, int fd2)
+void cat(int fd1, int fd2)
 {
   int n;
 
-  while((n = read(fd1, buf, sizeof(buf))) > 0) {
-    //write(1, buf, n);
-    printf(fd2, "%s", buf);
-  }
-  if(n < 0){
-    printf(1, "copy: read error\n");
-    exit();
-  }
+  while((n = read(fd1, buf, sizeof(buf))) > 0)
+	{
+ 		   printf(fd2, "%s", buf);
+ 	}
+  if(n < 0)
+	{
+    		printf(1, "copy: read error\n");
+    		exit();
+  	}
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  int fd1;
-  if(argc < 3){
-    printf(1, "copy: read error\n");
-    exit();
-  }
-  if((fd1 = open(argv[1], 0)) < 0){
-    printf(1, "copy: cannot open %s\n", argv[1]);
-    exit();
-  }
-  int fd2;
-  
-  if ((fd2 = open(argv[2], O_CREATE | O_RDWR)) < 0) {
-    printf(1, "copy: error initializing file: %s\n", argv[2]);
-    exit();
-  }
-  cat(fd1,fd2);
-  close(fd1);
-  close(fd2);
-  exit();
+  int fd1,fd2;
+  if(argc < 3)
+	{
+	    printf(1, "copy: read error\n");
+	    exit();
+	}
+  if((fd1 = open(argv[1], 0)) < 0)
+	{
+    		printf(1, "copy: cannot open %s\n", argv[1]);
+    		exit();
+  	}
+  if((fd2 = open(argv[2], O_CREATE | O_RDWR)) < 0) 
+	{
+    		printf(1, "copy: error initializing file: %s\n", argv[2]);
+    		exit();
+  	}
+ 	cat(fd1,fd2);
+ 	close(fd1);
+	close(fd2);
+	exit();
 }
--- touch.c
+++ touch.c
@@ -7,7 +7,6 @@ int main(int argc,char *argv[])
 {
 	int i;
 	int inputfile;
-
 	for(i = 1;i<=argc;i++)
 	{
 		inputfile = open(argv[i],O_CREATE | O_RDWR);

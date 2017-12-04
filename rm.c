898e033c75c600992e04e155683e4fde4de2b36c
farizputradandi <31751153+farizputradandi@users.noreply.github.com>
2017-12-04 23:16:22 +0700

    Add files via upload

--- rm.c
+++ rm.c
@@ -5,13 +5,19 @@ int main(int argc, char *argv[])
 {
   int i;
 
-  if(argc < 2){
+  if(argc == 1){
     printf(2, "Usage: rm files...\n"); 
     exit();
   }
+  else if(argc == 2){
+      printf(2, "Usage: rm folder...\n"); 
+    exit();
+  
+  }
 
   for(i = 1; i < argc; i++){
-	if (unlink(argv[i]) == 0){
+
+	if (unlink(argv[2]) == 0){
 		 printf(1, "berhasil mendelete file %s\n",argv[i]);}
 	 else{
      		 printf(1, "gagal mendelete file %s\n",argv[i]);}

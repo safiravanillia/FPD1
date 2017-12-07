cb999ec50b90388c55caf4f48b3d002caa15fec2
safiravanillia <32903307+safiravanillia@users.noreply.github.com>
2017-12-07 08:04:47 +0700

    fixed, cuman bisa hapus file, folder blm

--- rm.c
+++ rm.c
@@ -1,28 +1,19 @@
 #include "types.h"
 #include "stat.h"
-#include "user.h"  
-int main(int argc, char *argv[])
-{
-  int i;
+#include "user.h"
 
-  if(argc == 1){
-    printf(2, "Usage: rm files...\n"); 
-    exit();
-  }
-  else if(argc == 2){
-      printf(2, "Usage: rm folder...\n"); 
-    exit();
-  
-  }
+int main(int argc, char *argv[]){
+	int i;
+	if (argc < 2) {
+		printf(2, "Usage: rm files...\n");
+		exit();
+	}
 
-  for(i = 1; i < argc; i++){
-
-	if (unlink(argv[2]) == 0){
-		 printf(1, "berhasil mendelete file %s\n",argv[i]);}
-	 else{
-     		 printf(1, "gagal mendelete file %s\n",argv[i]);}
- 
-     
-    } 
- exit();
+	for (i = 1; i < argc; i++) {
+		if (unlink(argv[i]) < 0) {
+			printf(2, "rm: %s failed to delete\n", argv[i]);
+			break;
+		}
+	}
+	exit();
 }

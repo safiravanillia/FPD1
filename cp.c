10a3722837ca2451f22942b47fdf4e6920d04297
safiravanillia <32903307+safiravanillia@users.noreply.github.com>
2017-12-09 06:33:04 +0700

    fungsi cp *,-r belum, tp dah bisa cp biasa beberapa file

--- cp.c
+++ cp.c
@@ -6,41 +6,55 @@
 
 char buf[512];
 
-void cat(int fd1, int fd2)
-{
+void cat(int fd1, int fd2){
   int n;
 
-  while((n = read(fd1, buf, sizeof(buf))) > 0)
-	{
- 		   printf(fd2, "%s", buf);
- 	}
-  if(n < 0)
-	{
-    		printf(1, "copy: read error\n");
-    		exit();
-  	}
+  while((n = read(fd1, buf, sizeof(buf))) > 0) {
+    //write(1, buf, n);
+    printf(fd2, "%s", buf);
+  }
+  if(n < 0){
+    printf(1, "copy: read error\n");
+    exit();
+  }
 }
 
-int main(int argc, char *argv[])
-{
-  int fd1,fd2;
-  if(argc < 3)
-	{
-	    printf(1, "copy: read error\n");
-	    exit();
-	}
-  if((fd1 = open(argv[1], 0)) < 0)
-	{
+int main(int argc, char *argv[]){
+  int fd1,fd2,i;
+  if(argc < 3){
+    	printf(1, "copy: read error\n");
+    	exit();
+  }
+  
+  //fungsi cp -r nya masih copy biasa
+  if(argc == 4 && strcmp(argv[1],"-R")==0){
+  	if((fd1 = open(argv[2], 0)) < 0){
     		printf(1, "copy: cannot open %s\n", argv[1]);
     		exit();
   	}
-  if((fd2 = open(argv[2], O_CREATE | O_RDWR)) < 0) 
-	{
+  	if ((fd2 = open(argv[3], O_CREATE | O_RDWR)) < 0) {
     		printf(1, "copy: error initializing file: %s\n", argv[2]);
     		exit();
   	}
- 	cat(fd1,fd2);
- 	close(fd1);
-	close(fd2);
+  	cat(fd1,fd2);
+	close(fd1);
+  	close(fd2);
+  	exit();
+  } //cp biasa : copy 1/lebih file
+  else if(argc>=3){
+	for(i=1;i<argc-1;i++){
+		if((fd1 = open(argv[i], 0)) < 0){
+    			printf(1, "copy: cannot open %s\n", argv[1]);
+    			exit();
+		}
+		if ((fd2 = open(argv[argc], O_CREATE | O_RDWR)) < 0) {
+    			printf(1, "copy: error initializing file: %s\n", argv[2]);
+    			exit();
+  		}
+		cat(fd1,fd2);
+		close(fd1);
+  		close(fd2);
+  	}
 	exit();
+  }
 }

f275dbfd9dee46858144076a63462ce61df8dc61
safiravanillia <32903307+safiravanillia@users.noreply.github.com>
2017-12-09 06:39:36 +0700

    ngerapiin

--- rm.c
+++ rm.c
@@ -1,13 +1,10 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"    
-int main(int argc, char *argv[])
-{ 
-
-
+int main(int argc, char *argv[]){ 
   int i ; 
   if(argc == 1 && strcmp(argv[0],"rm")==0 ){
-    printf(2, "Usage: rm files... %s\n",argv[0]); 
+    printf(2, "Usage: rm files atau folder... %s\n",argv[0]); 
     exit();
   }
   else if(argc == 2 && strcmp(argv[0],"rm")==0 && strcmp(argv[1],"-r")==0){
@@ -15,16 +12,9 @@ int main(int argc, char *argv[])
     exit();
   
   }
- 
   for(i = 1; i < argc; i++){
-
-
-
 	if (unlink(argv[i]) == 0){
-		 printf(1, "berhasil mendelete file %s\n",argv[i]);}
-	  
- 
-     
+		 printf(1, "berhasil menghapus %s\n",argv[i]);
     } 
  exit();
 }

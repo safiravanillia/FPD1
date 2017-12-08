9a27f6a1ceaf803e9d7eebf4ca1cf3b9eca95495
safiravanillia <32903307+safiravanillia@users.noreply.github.com>
2017-12-09 06:34:46 +0700

    ngerapiin

--- sleep.c
+++ sleep.c
@@ -2,12 +2,12 @@
 #include "stat.h"
 #include "user.h"
 
-int main(int n, char *chr[]){
-	int p,m;
-	p = atoi(chr[1]);
-	m = p*10000/147;
-	printf(1,"wait for %d seconds -> ",p);
-	sleep(m);
+int main(int argc, char *argv[]){
+	int convert,hasil;
+	convert = atoi(argv[1]);
+	hasil = convert*10000/147;
+	printf(1,"wait fot %d seconds ->",convert);
+	sleep(hasil);
 	printf(1,"wait is over\n");
 	exit();
 }
